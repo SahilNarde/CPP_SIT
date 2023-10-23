52b2b5746c7592ed3a749d2cd464264f0722c2ef
Sahil Narde <sahilnarde@gmail.com>
2023-10-23 16:14:15 +0530

    update

--- Sorting Algorithms/insertion_sort.cpp
+++ Sorting Algorithms/insertion_sort.cpp
@@ -8,4 +8,22 @@ int main()
 {
     int n = 7;
     int arr[n] = {23, 11, 35, 9, 62, 51, 45};
+    int key;
+    int j;
+    for(int i=0;i<n;i++)
+    {
+        key = arr[i];
+        j=i-1;
+        while (j>=0 and arr[j]>key)
+        {
+            arr[j+1] = arr[j];
+            j--;
+        }
+        arr[j+1]=key;        
+    }
+    cout << "array:\n";
+    for (int i = 0; i < n - 1; i++)
+    {
+        cout << arr[i] << endl;
+    }
 }
